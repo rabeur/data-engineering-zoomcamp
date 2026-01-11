9deb6f19d693b0218f63e5e316f691288574dfe4
arabier <rabiera69@gmail.com>
2026-01-11 19:02:17 +0000

    test GCP VM vscode git

--- 04-dataengineering-zoomcamp/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-dataengineering-zoomcamp/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -19,7 +19,7 @@ select
     {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
     
-    -- timestamps
+    -- timestampss
     cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
